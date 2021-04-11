dfb0a6053b86e6235bc4967ee4e237cad0c979de
jigyasa08 <jkaushik0802@gmail.com>
2021-04-11 22:32:14 +0530

    Schema for MovieAPI created

---
+++ MovieAPI/models/Movie.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema(
+  {
+  
+    name: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model("Movie", movieSchema);
