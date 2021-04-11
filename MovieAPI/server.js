57cc02a9a95ab6a3b56a56d2e73a4220a6e3f35f
unknown <jkaushik0802@gmail.com>
2021-04-11 22:21:57 +0530

    Created MVC structurefor MovieAPI and created function for database connection

--- MovieAPI/server.js
+++ MovieAPI/server.js
@@ -6,6 +6,9 @@ app.use(cors());
 
 app.use(express.json());
 
+const connectDB = require("./config/db");
+connectDB();
+
 app.listen(8000, () => {
   console.log(`The server is up and running.`);
 });
